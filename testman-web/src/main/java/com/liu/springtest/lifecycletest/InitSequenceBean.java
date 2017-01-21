01937031adf56c026f860277ff8b15e5d979d92e
liuzhilei <liuzhilei@jd.com>
2017-01-21 14:56:00 +0800

    springbean生命周期

---
+++ testman-web/src/main/java/com/liu/springtest/lifecycletest/InitSequenceBean.java
@@ -0,0 +1,51 @@
+package com.liu.springtest.lifecycletest;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.config.AbstractFactoryBean;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Created by liuzhilei on 2017/1/18.
+ * spring允许在bean初始化之后，销毁之前执行特性的操作，常用的方式有三种：
+ * 1.通过实现InitializingBean、DisposableBean接口来决定初始化之后、销毁之前的操作
+ * 2.通过<bean>元素的init-method、destroy-method属性决定初始化之后，销毁之前的操作
+ * 3.在指定方法上定义@PostConstruct和@PreDestroy决定初始化之后，销毁之前的操作
+ *
+ *
+ *
+ *
+ * 验证上面三个的区别
+ */
+public class InitSequenceBean extends AbstractFactoryBean {
+
+    public InitSequenceBean() {
+        System.out.println("construct");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("afterPropertiesSet");
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return InitSequenceBean.class;
+    }
+
+    @Override
+    protected Object createInstance() throws Exception {
+        return new InitSequenceBean();
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println("postConstruct");
+    }
+
+    public void initMethod() {
+        System.out.println("init-method");
+    }
+
+}
---
+++ testman-web/src/main/java/com/liu/springtest/lifecycletest/Test.java
@@ -0,0 +1,14 @@
+package com.liu.springtest.lifecycletest;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by liuzhilei on 2017/1/19.
+ */
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-test.xml");
+        applicationContext.getBean("initSequenceBean");
+    }
+}
