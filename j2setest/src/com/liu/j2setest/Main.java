722b8e148ed8a8e69085f883289247fddc4e4c1b
liuzhilei <liuzhilei@jd.com>
2017-03-31 22:15:37 +0800

    JVM

--- j2setest/src/com/liu/j2setest/Main.java
+++ j2setest/src/com/liu/j2setest/Main.java
@@ -8,12 +8,20 @@ import java.util.concurrent.Executors;
 /**
  * Created by liuzhilei on 2017/1/10.
  */
-public class Main {
+ public class Main {
 
     public static void main(String[] args) {
+        String string = "";
         System.out.println(Runtime.getRuntime().availableProcessors());
     }
 
+
+
 }
 
-
+class Something {
+    public synchronized void isSyncA(){}
+    public synchronized void isSyncB(){}
+    public static synchronized void cSyncA(){}
+    public static synchronized void cSyncB(){}
+}
