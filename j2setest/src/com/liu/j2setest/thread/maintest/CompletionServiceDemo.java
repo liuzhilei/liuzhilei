4f19f73258c0a0beb375f7ac91418e4a9a1fa85a
liuzhilei <liuzhilei@jd.com>
2017-04-06 12:17:40 +0800

    completionService的应用

--- j2setest/src/com/liu/j2setest/thread/maintest/CompletionServiceDemo.java
+++ j2setest/src/com/liu/j2setest/thread/maintest/CompletionServiceDemo.java
@@ -6,6 +6,7 @@ import java.util.concurrent.*;
 /**
  * Created by liuzhilei on 2017/3/24.
  * <p/>
+ * CompletionService将executor和blockQueen融合到了一起
  * CompletionService是将线程池中所有线程的执行结果future放入blockQueen队列，take可以取出结果
  * 应用：对于多重验证，如果互相之间没有关联，可以应用CompletionService，利用线程池，提高性能,更快的处理返回结果
  */
