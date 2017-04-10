908843196faa68d1faab27cf771a2f5f276cdcfa
liuzhilei <liuzhilei@jd.com>
2017-04-10 11:59:14 +0800

    yield是线程由运行状态转到就绪状态

--- j2setest/src/com/liu/j2setest/thread/java多线程编程核心技术/第一章java多线程技能/yield/YieldTest.java
+++ j2setest/src/com/liu/j2setest/thread/java多线程编程核心技术/第一章java多线程技能/yield/YieldTest.java
@@ -2,16 +2,17 @@ package com.liu.j2setest.thread.java多线程编程核心技术.第一章java多
 
 /**
  * Created by liuzhilei on 2017/3/15.
+ * yield 使线程由“运行状态”转到“就绪状态”，不会释放锁
  */
 public class YieldTest extends Thread {
 
     @Override
     public void run() {
         long start = System.currentTimeMillis();
-        for (int i = 0; i < 500000; i++) {
-            //Thread.yield();
-            //System.out.println("i:" + i);
-        }
+        for (int i = 0; i < 50000; i++) {
+            Thread.yield();
+            System.out.println("i = " + i);
+    }
         long end = System.currentTimeMillis();
         System.out.println("用时：" + (end - start));
     }
@@ -23,5 +24,8 @@ class YieldMain {
         Thread thread = new YieldTest();
         thread.setPriority(2);
         thread.start();
+
+        Thread thread1 = new YieldTest();
+        thread1.start();
     }
 }
