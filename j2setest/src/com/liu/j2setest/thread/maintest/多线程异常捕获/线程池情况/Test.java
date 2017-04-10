bfb52cd499e4904c02816b7a460e9ab26aec1adb
liuzhilei <liuzhilei@jd.com>
2017-04-10 17:21:14 +0800

    runnable没有返回值，callable有返回值

--- j2setest/src/com/liu/j2setest/thread/maintest/多线程异常捕获/线程池情况/Test.java
+++ j2setest/src/com/liu/j2setest/thread/maintest/多线程异常捕获/线程池情况/Test.java
@@ -15,12 +15,23 @@ public class Test {
 
         Future<?> submit = executorService.submit(threadTest);
         try {
-            submit.get();
+            System.out.println("实现Runnable没有返回值，输出肯定是空：-------  " + submit.get());
         } catch (InterruptedException e) {
             System.out.println("22222");
             e.printStackTrace();
         } catch (ExecutionException e) {
             System.out.println("111111");
+            e.printStackTrace();
+        }
+
+        Future<Integer> submit1 = executorService.submit(new ThreadTest1());
+        try {
+            System.out.println("实现Callable有返回值，输出数值为：--------" + submit1.get());
+        } catch (InterruptedException e) {
+            System.out.println("333333");
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            System.out.println("444444");
             e.printStackTrace();
         }
 
@@ -33,7 +44,15 @@ class ThreadTest implements Runnable {
     @Override
     public void run() {
         int i = 1;
-        i = i / 0;
+        //i = i / 0;
+    }
+}
+
+class ThreadTest1 implements Callable<Integer> {
+    @Override
+    public Integer call() throws Exception {
+        int i = 1/0;
+        return 1;
     }
 }
 
