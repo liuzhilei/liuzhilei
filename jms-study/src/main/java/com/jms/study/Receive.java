4c5cd70d4ef18fa5ea0e7a7017951efcab1d2f07
liuzhilei <liuzhilei@jd.com>
2017-05-17 20:33:50 +0800

    jms

---
+++ jms-study/src/main/java/com/jms/study/Receive.java
@@ -0,0 +1,50 @@
+package com.jms.study;
+
+import org.springframework.stereotype.Service;
+
+import javax.jms.*;
+import javax.naming.InitialContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by liuzhilei on 2017/5/17.
+ */
+@Service
+public class Receive extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        try {
+            InitialContext context = new InitialContext();
+            Queue queue = (Queue) context.lookup("java:comp/env/queue/queue0");
+
+            QueueConnectionFactory connectionFactory = (QueueConnectionFactory) context.lookup("java:comp/env/queue/connectionFactory");
+
+            QueueConnection connection = connectionFactory.createQueueConnection();
+
+            QueueSession session = connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            QueueReceiver receiver = session.createReceiver(queue);
+
+            connection.start();
+
+            TextMessage message = (TextMessage) receiver.receive();
+
+            writer.write("receive：" + message.getText());
+            connection.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
---
+++ jms-study/src/main/java/com/jms/study/Send.java
@@ -0,0 +1,56 @@
+package com.jms.study;
+
+import org.springframework.stereotype.Service;
+
+import javax.jms.*;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by liuzhilei on 2017/5/17.
+ */
+@Service("/send")
+public class Send extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        try {
+            InitialContext context = new InitialContext();
+            Queue queue = (Queue) context.lookup("java:comp/env/queue/queue0");
+
+            QueueConnectionFactory connectionFactory = (QueueConnectionFactory) context.lookup("java:comp/env/queue/connectionFactory");
+
+            QueueConnection queueConnection = connectionFactory.createQueueConnection();
+
+            QueueSession queueSession = queueConnection.createQueueSession(false, Session.DUPS_OK_ACKNOWLEDGE);
+
+            QueueSender queueSender = queueSession.createSender(queue);
+            queueSender.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+            TextMessage textMessage = queueSession.createTextMessage("hello world");
+
+            queueSender.send(textMessage);
+
+            writer.write("message sent: " + textMessage.getText());
+
+            queueConnection.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
