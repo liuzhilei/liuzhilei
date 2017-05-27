53505522849fcf41f5243bbfd156191ce2efe5b0
liuzhilei <liuzhilei@jd.com>
2017-05-27 12:12:01 +0800

    dubbo引入

---
+++ jms-study/src/main/java/com/jms/study/发布订阅/test.java
@@ -0,0 +1,14 @@
+package com.jms.study.发布订阅;
+
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+
+/**
+ * Created by liuzhilei on 2017/5/25.
+ */
+public class test implements MessageListener {
+    @Override
+    public void onMessage(Message message) {
+    }
+}
