480b711df000f2ac99dcaa18746c436040ba65e3
liuzhilei <liuzhilei@jd.com>
2017-06-02 17:53:36 +0800

    spring的bean初始化过程

--- testman-web/src/main/java/com/liu/springtest/springbeanlifecycle/BeanTest.java
+++ testman-web/src/main/java/com/liu/springtest/springbeanlifecycle/BeanTest.java
@@ -9,6 +9,25 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class BeanTest {
 
+    public static void main(String[] args) {
+        System.out.println("Spring容器初始化");
+        System.out.println("=====================================");
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-bean-life-cycle.xml");
+
+        System.out.println("Spring容器初始化完毕");
+        System.out.println("=====================================");
+
+        System.out.println("从容器中获取bean");
+        BeanLifeCycleService beanLifeCycleService = (BeanLifeCycleService)context.getBean("beanLifeCycleService");
+        System.out.println("beanLifeCycleService name =" + beanLifeCycleService.getName());
+        System.out.println("=====================================");
+
+        context.close();
+
+        System.out.println("spring容器关闭");
+    }
+
     @Test
     public void beanTest(){
         System.out.println("Spring容器初始化");
