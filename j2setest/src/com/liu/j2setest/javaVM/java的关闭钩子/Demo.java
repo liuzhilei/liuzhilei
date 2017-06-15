a93982d9dd06448f2e320bf505e971110627a855
liuzhilei <liuzhilei@jd.com>
2017-06-15 20:51:50 +0800

    钩子函数，以及tomcat中的钩子

--- j2setest/src/com/liu/j2setest/javaVM/java的关闭钩子/Demo.java
+++ j2setest/src/com/liu/j2setest/javaVM/java的关闭钩子/Demo.java
@@ -28,4 +28,43 @@ public class Demo {
             e.printStackTrace();
         }
     }
+
+    /*
+    * tomcat中的Catalina类中有一个钩子内部类的定义：
+    *
+    protected class CatalinaShutdownHook extends Thread {
+        public void run(){
+            try {
+                if (getServer() != null) {
+                    Catalina.this.stop(); // 因为是Catalina的内部类，所以可以调用Catalina的内部方法，此处调用的stop,在tomcat关闭之前，就进行了相应的操作
+                }
+            } catch (Throwable ex) {
+                log.error(sm.getString("catalina.shutdownHookFail"), ex);
+            } finally {
+                // If JULI is used, shut JULI down *after* the server shuts down
+                // so log messages aren't lost
+                LogManager logManager = LogManager.getLogManager();
+                if (logManager instanceof ClassLoaderLogManager) {
+                    ((ClassLoaderLogManager) logManager).shutdown();
+                }
+            }
+        }
+    }
+
+    //tomcat在启动的时候会调用Catalina类中的start方法，所以钩子在tomcat启动的时候就被注册到了jvm虚拟机
+
+    if(useShutdownHook)
+
+    {
+        if (shutdownHook == null) {
+            shutdownHook = new CatalinaShutdownHook();
+        }
+        Runtime.getRuntime().addShutdownHook(shutdownHook);
+
+    }
+
+
+    */
+
+
 }
