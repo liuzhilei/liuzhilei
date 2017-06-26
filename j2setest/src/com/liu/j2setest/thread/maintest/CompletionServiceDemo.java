93a35023905ef26d588297845b9cbd8f6cc723c4
liuzhilei <liuzhilei@jd.com>
2017-06-26 21:21:08 +0800

    completionService完成服务，将Executor和BlockingQueue的功能融合到一起

--- j2setest/src/com/liu/j2setest/thread/maintest/CompletionServiceDemo.java
+++ j2setest/src/com/liu/j2setest/thread/maintest/CompletionServiceDemo.java
@@ -6,8 +6,8 @@ import java.util.concurrent.*;
 /**
  * Created by liuzhilei on 2017/3/24.
  * <p/>
- * CompletionService将executor和blockQueen融合到了一起
- * CompletionService是将线程池中所有线程的执行结果future放入blockQueen队列，take可以取出结果
+ * CompletionService将Executor和BlockingQueue的功能融合到了一起
+ * 将Callable任务提交给CompletionService执行，它将线程池中所有线程的执行结果future放入BlockingQueue队列，take或者poll可以取出结果
  * 应用：对于多重验证，如果互相之间没有关联，可以应用CompletionService，利用线程池，提高性能,更快的处理返回结果
  */
 public class CompletionServiceDemo {
@@ -32,7 +32,7 @@ public class CompletionServiceDemo {
         CompletionService completionService = new ExecutorCompletionService(executorService);
         try {
             for (int i = 0; i < 10; i++) {
-                //执行任务，将执行结果future放入了blockQueen队列中，take可以取出
+                //执行任务，将执行结果future放入了BlockingQueue队列中，take可以取出
                 completionService.submit(new Task(i));
             }
 
@@ -40,7 +40,7 @@ public class CompletionServiceDemo {
                 //take等待下一个结果并且返回future对象
                 //poll不等待，有结果就返回future对象，没有就返回null
                 //completionService.poll();
-                System.out.println(completionService.take().get() + " i = " + i);//从blockQueen队列中利用take取出结果
+                System.out.println(completionService.take().get() + " i = " + i);//从BlockingQueue队列中利用take取出结果
             }
             System.out.println("CompletionService 执行完了............");
         } catch (InterruptedException e) {
