d5c7e9f422a8aed7b983bb06836bca8424d2db16
Administrator <liuzhilei92@126.com>
2017-07-01 14:50:13 +0800

    compareTo具体比较，返回-1，当前对象排在前面

--- j2setest/src/com/liu/j2setest/compare/MainTest.java
+++ j2setest/src/com/liu/j2setest/compare/MainTest.java
@@ -1,16 +1,41 @@
 package com.liu.j2setest.compare;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by liuzhilei on 2016/12/29.
  */
 public class MainTest {
 
     public static void main(String[] args) {
-        Person p1 = new Person(1, "name1", 20);
         Person p2 = new Person(2, "name2", 19);
+        Person p3 = new Person(3, "name2", 30);
+        Person p1 = new Person(1, "name1", 20);
 
-        System.out.println("实现comparable接口，按年龄比较---");
-        System.out.println(p1.compareTo(p2));
+
+
+        List<Person> list = new ArrayList<Person>();
+        list.add(p1);
+        list.add(p2);
+        list.add(p3);
+
+        Collections.sort(list);
+
+        //System.out.println("实现comparable接口，按年龄比较---");
+        //System.out.println(p1.compareTo(p2));
+        System.out.println(list);
+        for(int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+
+
+
+
+
+
 
         System.out.println("实现comparator接口，策略模式，按学号比较---");
         PersonComparator personComparator = new PersonComparator();
--- j2setest/src/com/liu/j2setest/compare/Person.java
+++ j2setest/src/com/liu/j2setest/compare/Person.java
@@ -8,7 +8,7 @@ import java.util.Comparator;
  * Comparable在java.lang包下，类直接实现，实现compareTo方法即可
  * comparator在java.util下，需要重新写一个类实现comparator，是策略模式，不改变对象本身
  */
-public class Person implements Comparable {
+public class Person implements Comparable<Person> {
     private int num;//学号
     private String name;//姓名
     private int age;//年龄
@@ -43,9 +43,34 @@ public class Person implements Comparable {
         this.age = age;
     }
 
+    /**
+     * 当前对象 - 比较对象
+     * 返回-1 当前对象排在前面
+     * 返回1，当前对象排在后面
+     * @param o
+     * @return
+     */
     @Override
-    public int compareTo(Object o) {
-        return this.age - ((Person) o).getAge();
+    public int compareTo(Person o) {
+        int i = this.age - o.getAge();
+        System.out.println("当前对象：" + this.age);
+        System.out.println("比较的对象：" + o.getAge());
+        System.out.println("比较结果" + i);
+        if (i > 0) {
+            return 1;
+        } else if (i < 0) {
+            return -1;
+        }
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "num=" + num +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
 
