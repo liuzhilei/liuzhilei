0667d6441ac3979d21630566ab685400884fcf2a
Administrator <liuzhilei92@126.com>
2017-07-01 17:16:18 +0800

    线程池到达空闲等待时间，不会回收核心线程数

--- j2setest/src/com/liu/j2setest/thread/maintest/各种线程池/线程池的扩展/TimingMain.java
+++ j2setest/src/com/liu/j2setest/thread/maintest/各种线程池/线程池的扩展/TimingMain.java
@@ -2,6 +2,7 @@ package com.liu.j2setest.thread.maintest.各种线程池.线程池的扩展;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -9,13 +10,17 @@ import java.util.concurrent.TimeUnit;
  */
 public class TimingMain {
     public static void main(String[] args) {
-        ExecutorService executorService = new TimingThreadPool(5, 5, 60L,
+        /**
+         * 此处如果不将核心线程数设置为0，那么该线程池永远不会结束
+         * 因为超时时间针对的时超过核心线程数的线程。超时以后，会回收多余的线程，但是不会回收核心线程
+         */
+        ExecutorService executorService = new TimingThreadPool(0, 100, 10L,
                 TimeUnit.SECONDS, new LinkedBlockingDeque<Runnable>());
         executorService.execute(new ThreadTest("thread1"));
         executorService.execute(new ThreadTest("thread2"));
         executorService.execute(new ThreadTest("thread3"));
 
-        executorService.shutdown();
+        //executorService.shutdown();
 
     }
 
@@ -30,7 +35,7 @@ public class TimingMain {
         public void run() {
             try {
                 System.out.println(name);
-                Thread.sleep(3000);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
