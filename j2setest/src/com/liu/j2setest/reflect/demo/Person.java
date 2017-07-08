80f67edf58c5d077f930707afed06f42da125cc4
liuzhilei <liuzhilei92@126.com>
2017-07-08 14:59:29 +0800

    反射

---
+++ j2setest/src/com/liu/j2setest/reflect/demo/Person.java
@@ -0,0 +1,26 @@
+package com.liu.j2setest.reflect.demo;
+
+/**
+ * Created by liuzhilei on 2017/5/25.
+ */
+public class Person<T> {
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ j2setest/src/com/liu/j2setest/reflect/demo/Student.java
@@ -0,0 +1,17 @@
+package com.liu.j2setest.reflect.demo;
+
+/**
+ * Created by liuzhilei on 2017/5/25.
+ */
+public class Student extends Person<Student> {
+
+    private int classNumber;
+
+    public int getClassNumber() {
+        return classNumber;
+    }
+
+    public void setClassNumber(int classNumber) {
+        this.classNumber = classNumber;
+    }
+}
