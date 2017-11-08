854da05709eb9ce3dd7ffbf8f9768a487130bb38
liuzhilei <liuzhilei92@126.com>
2017-11-08 11:55:00 +0800

    java动态代理机制原理

--- j2setest/src/com/liu/j2setest/javaVM/jvm参数/test.java
+++ j2setest/src/com/liu/j2setest/javaVM/jvm参数/test.java
@@ -9,15 +9,15 @@ package com.liu.j2setest.javaVM.jvm参数;
  * 70    3             java.lang.String::indexOf (70 bytes)
  * 78    4             sun.misc.ASCIICaseInsensitiveComparator::compare (126 bytes)
  * <p/>
- * 如果使用-Xint，就不会打印上面数据，因为就不会编译成机器码
+ * 如果使用-Xint，就不会打印上面数据，因为就不会编译成机器码，使用字节码解释运行
  * 如果使用-Xcomp,会输出更多的编译过程，因为会先编译成机器码，然后再执行
  * <p/>
  * -XX:+CITime显示编译时间
  * -XX:+PrintCompilation 和 -XX:+CITime 可以一起使用，更好的理解JIT编译器
  * 如果配置为 -Xint -XX:+PrintCompilation -XX:+CITime，那么编译时间会显示为0，因为是字节码解释运行，不需要编译
  *
- * C1就是普通编译，编译成字节码
- * C2就是jit编译，编译成机器码
+ * C1就是普通编译，使用字节码解释运行（java解释器）
+ * C2就是jit编译，编译成机器码（即时编译期）
  */
 public class test {
     public static void main(String[] args) {
