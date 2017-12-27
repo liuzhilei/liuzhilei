a56b6b5e8840a7113105d269e5447796a0e25f22
liuzhilei <liuzhilei92@126.com>
2017-12-27 11:08:07 +0800

    内部接口

--- j2setest/src/com/liu/j2setest/内部类和内部接口/innerInterface/outerInterface.java
+++ j2setest/src/com/liu/j2setest/内部类和内部接口/innerInterface/outerInterface.java
@@ -3,6 +3,8 @@ package com.liu.j2setest.内部类和内部接口.innerInterface;
 /**
  * Created by liuzhilei on 2017/4/12.
  * 内部接口，可以增强代码的易读性和可维护性
+ * 因为如果需要额外的操作，就可以实现这个接口，如果不需要，就不需要写。如果是抽象方法的话，不管需要不需要，都要写
+ * 其实我感觉在抽象类里面写一个方法，空实现也是可以的
  */
 public class outerInterface {
 
