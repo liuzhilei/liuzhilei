13b1fd524c014710cb4e6dd47a04d54a9fbfc62f
liuzhilei <liuzhilei92@126.com>
2018-04-24 21:53:05 +0800

    以k为一组反转单链表，最后不足K个节点的也反转

---
+++ j2setest/src/com/liu/j2setest/mianshiTest/Test5.java
@@ -0,0 +1,61 @@
+package com.liu.j2setest.mianshiTest;
+
+/**
+ * Created by liuzhilei on 2018/4/24.
+ * 以k为一组反转单链表，最后不足K个节点的也反转
+ */
+public class Test5 {
+
+    class Node {
+        Node next;
+        Integer value;
+    }
+
+    public static void main(String[] args) {
+        Node node = new Test5().new Node();
+        node.value = 5;
+
+        node.next = new Test5().new Node();
+        node.next.value = 6;
+
+        node.next.next = new Test5().new Node();
+        node.next.next.value = 3;
+
+        node.next.next.next = new Test5().new Node();
+        node.next.next.next.value = 7;
+
+        node.next.next.next.next = new Test5().new Node();
+        node.next.next.next.next.value = 2;
+
+        node.next.next.next.next.next = new Test5().new Node();
+        node.next.next.next.next.next.value = 8;
+
+        node.next.next.next.next.next.next = new Test5().new Node();
+        node.next.next.next.next.next.next.value = 1;
+
+        Node node1 = reverseNode(node, 3);
+        System.out.println(node1);
+    }
+
+    public static Node reverseNode(Node node, int k) {
+        Node b = null;
+        Node cur = node;
+        int count = 0;
+        while (count < k && cur != null) {
+            Node a = cur.next;
+            cur.next = b;
+            b = cur;
+            cur = a;
+            count++;
+        }
+
+        //分组执行，每次走到这就是走完了一个K组,把本组的最后一个值的next指向下一组转换后的第一个值，就联合成一条链表了
+        if (cur != null) {
+            node.next= reverseNode(cur, 3);
+        }
+
+        return b;
+    }
+
+
+}
