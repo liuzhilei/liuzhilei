5216f5b197b546d8e3718bae29c7d25c18f71ec0
liuzhilei@126.com <liulei121>
2018-06-07 22:43:42 +0800

    单例模式的真正写法，是序列化都能得到单例

--- j2setest/src/com/liu/j2setest/designpattern/SingleTon/Main.java
+++ j2setest/src/com/liu/j2setest/designpattern/SingleTon/Main.java
@@ -1,5 +1,7 @@
 package com.liu.j2setest.designpattern.SingleTon;
 
+import java.io.*;
+
 /**
  * Created by liuzhilei on 2017/7/21.
  */
@@ -7,8 +9,25 @@ public class Main {
 
     public static void main(String[] args) {
         SingleTon5 singleTon5 = SingleTon5.getInstance();
-        SingleTon5 singleTon51 = SingleTon5.getInstance();
 
-        System.out.println(singleTon5 == singleTon51);
+        try {
+            System.out.println(singleTon5);
+
+            File file = new File("111.out");
+            ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(file));
+            outputStream.writeObject(singleTon5);
+
+
+            System.out.println("单例模式定义了readResolve方法，就可以保证序列化前后得到的都是同一个对象，真正实现了单例");
+            System.out.println("==从文件读出来的实体==");
+            ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(file));
+            SingleTon5 singleTest1 = (SingleTon5) inputStream.readObject();
+
+            System.out.println(singleTest1);
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
--- j2setest/src/com/liu/j2setest/designpattern/SingleTon/SingleTon5.java
+++ j2setest/src/com/liu/j2setest/designpattern/SingleTon/SingleTon5.java
@@ -1,12 +1,16 @@
 package com.liu.j2setest.designpattern.SingleTon;
 
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+
 /**
  * Created by liuzhilei on 2017/1/6.
  * 静态内部类形式的单例模式
  * 单例模式的最优解决办法
  */
-public class SingleTon5 {
+public class SingleTon5 implements Serializable{
 
+    //静态内部类，只有在第一次被引用的时候才会加载，而且放入了jvm的方法区，天然保证了线程安全
     private static class SingleTonHolder {
         //静态初始化，jvm保证线程安全
         private static SingleTon5 instance = new SingleTon5();
@@ -14,9 +18,24 @@ public class SingleTon5 {
 
     //私有化构造器
     private SingleTon5() {
+        if(SingleTonHolder.instance != null){
+            throw new RuntimeException("单例模式不允许反射侵入");
+        }
     }
 
     public static SingleTon5 getInstance() {
+        return SingleTonHolder.instance;
+    }
+
+    /**
+     * 如果不写这个方法，那么单例就不能发挥真正的作用，因为正常情况下单例通过序列化反序列以后，得到的肯定不是同一个对象
+     * 写了这个方法，当反序列化的时候，会默认调用ObjectInputStream.readObject方法，这个里面会通过反射调用到自定义的readResolve方法，
+     * 在readResolve方法中返回这个单例的对象，那么就可以保证任何情况都可以真正的实现单例模式了
+     * 而且类一定要实现序列化，要不然怎么会序列化呢
+     * @return
+     * @throws ObjectStreamException
+     */
+    private Object readResolve() throws ObjectStreamException {
         return SingleTonHolder.instance;
     }
 
