cd8c23418474b0ce18eb3b5f5de29940737ef18d
liuzhilei92@126.com <liuzhilei92@126.com>
2018-06-27 11:12:43 +0800

    修改Main方法

--- j2setest/src/com/liu/j2setest/Main.java
+++ j2setest/src/com/liu/j2setest/Main.java
@@ -37,30 +37,6 @@ public class Main {
         return i;
     }
 
-    public static void compareTable() {
-        String str = "CREATE TABLE `sdkChuguanStock_compare_task_%s` (\n" +
-                "  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',\n" +
-                "  `sku` bigint(20) NOT NULL COMMENT 'sku',\n" +
-                "  `chuguanNum` mediumtext NOT NULL COMMENT '出管现货在途，以及内配系统的数据',\n" +
-                "  `stockNum` mediumtext NOT NULL COMMENT 'sdk数据',\n" +
-                "  `chuguan_sub_stock` mediumtext NOT NULL COMMENT 'chuguan和sdk差异值',\n" +
-                "  `stable_count` int(11) NOT NULL COMMENT '稳定次数',\n" +
-                "  `compared_count` int(11) NOT NULL COMMENT '对比次数',\n" +
-                "  `status` smallint(6) NOT NULL COMMENT '任务状态',\n" +
-                "  `status_desc` varchar(20) NOT NULL COMMENT '任务状态描述',\n" +
-                "  `createtime` datetime NOT NULL COMMENT '创建时间',\n" +
-                "  `modifytime` datetime NOT NULL COMMENT '修改时间',\n" +
-                "  PRIMARY KEY (`id`),\n" +
-                "  UNIQUE KEY `idx_compare_sku` (`sku`),\n" +
-                "  KEY `idx_compare_status` (`status`)\n" +
-                ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='自营和出管现货、在途、内配出入对比任务表';";
-
-        for (int i = 0; i < 128; i++) {
-            String format = String.format(str, i);
-            System.out.println(format);
-            System.out.println();
-        }
-    }
 
     public static void repairTable() {
         String str = "CREATE TABLE `sdkChuguanStock_repair_task_%s` (\n" +
