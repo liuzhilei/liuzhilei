7991fdd74d64c50e97dcac0d0240122831629e8f
liuzhilei <liuzhilei92@126.com>
2018-08-20 20:09:51 +0800

    unSafe操作

---
+++ j2setest/src/com/liu/j2setest/unfase/Player.java
@@ -0,0 +1,28 @@
+package com.liu.j2setest.unfase;
+
+/**
+ * Created by liuzhilei3 on 2018/8/20.
+ */
+public class Player {
+    private String name;
+
+    private int age;
+
+    private Player(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
