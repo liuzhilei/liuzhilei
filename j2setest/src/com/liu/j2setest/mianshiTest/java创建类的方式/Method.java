73b4d1591a03047642c575ac830b97aa4eb8a52c
liuzhilei <liuzhilei92@126.com>
2018-08-20 20:44:33 +0800

    java创建类的六种方式

---
+++ j2setest/src/com/liu/j2setest/mianshiTest/java创建类的方式/Method.java
@@ -0,0 +1,37 @@
+package com.liu.j2setest.mianshiTest.java创建类的方式;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+
+/**
+ * Created by liuzhilei3 on 2018/8/20.
+ */
+public class Method {
+
+    public static void main(String[] args) throws Exception {
+        //1.new类
+        NewClassDemo demo1 = new NewClassDemo();
+
+        /**2.反射 */
+        //2.1 利用Class的newInstance
+        NewClassDemo demo2 = NewClassDemo.class.newInstance();
+
+        //2.2 利用Constructor的newInstance
+        Constructor constructor = NewClassDemo.class.getConstructor();
+        NewClassDemo demo3 = (NewClassDemo) constructor.newInstance();
+
+        //3.序列化反序列化
+        // 详见serializable包
+
+        //4.利用unsafe，直接通过内存偏移地址获取，不会调用构造方法
+        Field f = Unsafe.class.getDeclaredField("theUnsafe");
+        f.setAccessible(true);
+        Unsafe unsafe = (Unsafe) f.get(null);
+        NewClassDemo demo4 = (NewClassDemo) unsafe.allocateInstance(NewClassDemo.class);
+
+        //5.clone,不会调用构造方法
+        NewClassDemo demo5 = (NewClassDemo) demo3.clone();
+    }
+}
