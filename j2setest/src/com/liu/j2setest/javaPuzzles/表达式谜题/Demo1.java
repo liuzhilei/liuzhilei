37cf4b4a284d2a3d34711e87879e6d9a6c416cd7
liuzhilei <liuzhilei92@126.com>
2018-09-12 20:54:43 +0800

    判断一个数为奇数，负数的最佳方式

--- j2setest/src/com/liu/j2setest/javaPuzzles/表达式谜题/Demo1.java
+++ j2setest/src/com/liu/j2setest/javaPuzzles/表达式谜题/Demo1.java
@@ -13,8 +13,19 @@ public class Demo1 {
     /**
      * i % 2 != 0也可以
      * 不要使用i % 2 == 1，因为参数有可能是负数
+     * 判断一个数是否为奇数
      */
     public static boolean isOdd(int i) {
         return (i & 1) != 0;
     }
+
+    /**
+     * 判断一个数是否为正数
+     * @param i
+     * @return
+     */
+    private static boolean isZhengShu(int i) {
+        return (i >> 31) == 0;
+    }
+
 }
